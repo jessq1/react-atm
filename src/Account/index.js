84cd4161d709cf02512bef5b82fb6420b1422806
JessQi <85958041+jessq1@users.noreply.github.com>
2021-08-03 17:51:02 -0400

    complete lab

--- src/Account/index.js
+++ src/Account/index.js
@@ -1,17 +1,54 @@
-import React, { Component } from 'react';
-
+import  React, { Component } from 'react';
 
 class Account extends Component {
-  render() {
+  constructor(props){
+    super()
+    this.textInput = 0;
+    this.state = {
+      balance: 0,
+    }
+    this.setTextInputRef = element => {
+      this.textInput = element;
+    };
+  }
+
+Deposit = () => {
+  console.log(this.textInput)
+      let balance = this.state.balance + parseInt(this.textInput.value);
+      this.setState({
+        balance: balance
+      });
+};
+Withdraw = () => {
+    let balance = this.state.balance - parseInt(this.textInput.value);
+    this.setState({
+      balance: balance
+   });
+};
+
+  render(){
+    if (this.state.balance === 0){
+      return (
+        <div className="account">
+          <h2>{this.props.name}</h2>
+          <div className="balance zero">${this.state.balance}</div>
+          <input type="text" placeholder="enter an amount"  ref={this.setTextInputRef}/>
+          <input type="button" value="Deposit" onClick={this.Deposit} />
+          <input type="button" value="Withdraw" onClick={this.Withdraw} />
+        </div>
+      )
+    }else {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount"  ref={this.setTextInputRef}/>
+        <input type="button" value="Deposit" onClick={this.Deposit} />
+        <input type="button" value="Withdraw" onClick={this.Withdraw} />
       </div>
-    )
+)
+
+    }
   }
 }
 
